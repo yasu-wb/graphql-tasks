5e05dc64c824eaa9e5c8c5faf9269ad32466aee8
yasu-wb <yshr.kihara@gmail.com>
2025-06-01 21:58:12 +0900

    Create Task Model

---
+++ backend/src/task/models/task.model.ts
@@ -0,0 +1,19 @@
+import { ObjectType, Field, Int } from '@nestjs/graphql';
+
+@ObjectType()
+export class Task {
+  @Field(() => Int)
+  id: number;
+
+  @Field()
+  name: string;
+
+  @Field()
+  dueDate: string;
+
+  @Field()
+  status: 'NOT_STARTED' | 'IN_PROGRESS' | 'COMPLETED';
+
+  @Field({ nullable: true })
+  description: string;
+}
