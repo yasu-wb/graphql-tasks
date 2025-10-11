fc6ced9766787c6f971b0fb568aa8a6a6cf97ba2
yasu-wb <yshr.kihara@gmail.com>
2025-10-11 14:34:48 +0900

    feat: create user

--- backend/src/user/user.resolver.ts
+++ backend/src/user/user.resolver.ts
@@ -1,4 +1,17 @@
-import { Resolver } from '@nestjs/graphql';
+import { Args, Mutation, Resolver } from '@nestjs/graphql';
+import { UserService } from './user.service';
+import { User } from '@prisma/client';
+import { CreateUserInput } from './dto/createUser.input';
+import { User as UserModel } from './models/user.model';
 
 @Resolver()
-export class UserResolver {}
+export class UserResolver {
+  constructor(private readonly userService: UserService) {}
+
+  @Mutation(() => UserModel)
+  async createUser(
+    @Args('createUserInput') createUserInput: CreateUserInput,
+  ): Promise<User> {
+    return await this.userService.createUser(createUserInput);
+  }
+}
