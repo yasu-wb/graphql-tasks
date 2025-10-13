3a6a358e4b27d73caf08ae3bed9801f73dbffe24
yasu-wb <yshr.kihara@gmail.com>
2025-10-13 19:50:46 +0900

    feat: Add Auth Module & jwt Module

--- backend/src/user/user.module.ts
+++ backend/src/user/user.module.ts
@@ -6,5 +6,6 @@ import { PrismaModule } from '../prisma/prisma.module';
 @Module({
   imports: [PrismaModule],
   providers: [UserResolver, UserService],
+  exports: [UserService],
 })
 export class UserModule {}
