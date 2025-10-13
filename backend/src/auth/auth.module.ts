479704dd29df315f672d6d20e9598a6b85d5ff85
yasu-wb <yshr.kihara@gmail.com>
2025-10-13 20:32:57 +0900

    feat: Sign In

--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -4,6 +4,7 @@ import { AuthService } from './auth.service';
 import { UserModule } from 'src/user/user.module';
 import { PassportModule } from '@nestjs/passport';
 import { JwtModule } from '@nestjs/jwt';
+import { LocalStrategy } from './strategies/local.strategy';
 
 @Module({
   imports: [
@@ -14,6 +15,6 @@ import { JwtModule } from '@nestjs/jwt';
       signOptions: { expiresIn: '1h' },
     }),
   ],
-  providers: [AuthResolver, AuthService],
+  providers: [AuthResolver, AuthService, LocalStrategy],
 })
 export class AuthModule {}
--- backend/src/auth/auth.resolver.ts
+++ backend/src/auth/auth.resolver.ts
@@ -1,4 +1,20 @@
-import { Resolver } from '@nestjs/graphql';
+import { Args, Context, Mutation, Resolver } from '@nestjs/graphql';
+import { AuthService } from './auth.service';
+import { SignInResponse } from './dto/signInResponse';
+import { UseGuards } from '@nestjs/common';
+import { GqlAuthGuard } from './guards/gql-auth.guard';
+import { SignInInput } from './dto/singIn.input';
 
 @Resolver()
-export class AuthResolver {}
+export class AuthResolver {
+  constructor(private readonly authService: AuthService) {}
+
+  @Mutation(() => SignInResponse)
+  @UseGuards(GqlAuthGuard)
+  async signIn(
+    @Args('signInInput') signInInput: SignInInput,
+    @Context() context: any,
+  ) {
+    return await this.authService.signIn(context.user);
+  }
+}
