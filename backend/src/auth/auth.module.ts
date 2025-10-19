129d9ac0c49c6d0b6c6a5613e2e857396f4844c1
yasu-wb <yshr.kihara@gmail.com>
2025-10-19 20:06:24 +0900

    feat: JWT Authorization

--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -5,6 +5,7 @@ import { UserModule } from 'src/user/user.module';
 import { PassportModule } from '@nestjs/passport';
 import { JwtModule } from '@nestjs/jwt';
 import { LocalStrategy } from './strategies/local.strategy';
+import { JwtStrategy } from './strategies/jwt.strategy';
 
 @Module({
   imports: [
@@ -15,6 +16,6 @@ import { LocalStrategy } from './strategies/local.strategy';
       signOptions: { expiresIn: '1h' },
     }),
   ],
-  providers: [AuthResolver, AuthService, LocalStrategy],
+  providers: [AuthResolver, AuthService, LocalStrategy, JwtStrategy],
 })
 export class AuthModule {}
--- backend/src/user/user.resolver.ts
+++ backend/src/user/user.resolver.ts
@@ -4,6 +4,8 @@ import { User } from '@prisma/client';
 import { CreateUserInput } from './dto/createUser.input';
 import { User as UserModel } from './models/user.model';
 import { GetUserArgs } from './dto/getUser.args';
+import { UseGuards } from '@nestjs/common';
+import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
 
 @Resolver()
 export class UserResolver {
@@ -17,6 +19,7 @@ export class UserResolver {
   }
 
   @Query(() => UserModel, { nullable: true })
+  @UseGuards(JwtAuthGuard)
   async getUser(@Args() getUserArgs: GetUserArgs): Promise<User | null> {
     return await this.userService.getUser(getUserArgs.email);
   }
