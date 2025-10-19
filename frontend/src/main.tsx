bbd78c1b4791cc459673049e06cebde5a41b803e
yasu-wb <yshr.kihara@gmail.com>
2025-10-19 20:39:53 +0900

    feat: frontend setup

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,6 +3,8 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
+import 'modern-css-reset';
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
