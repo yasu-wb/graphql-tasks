9ab13e152d10f0ed350b0f8201498e5207aac490
yasu-wb <yshr.kihara@gmail.com>
2025-10-25 16:36:51 +0900

    feat: React Router

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,21 @@
 import './App.css'
+import Main from './components/Main'
+import NotFound from './components/NotFound'
+import SignIn from './components/SignIn'
+import SignUp from './components/SignUp'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 function App() {
 
   return (
-    <>
-      <h1>Hello World</h1>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/signin'  element={<SignIn />} />
+        <Route path='signup' element={<SignUp />} />
+        <Route path='/' element={<Main/>} />
+        <Route path='*' element={<NotFound/>} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
